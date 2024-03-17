6dd5900f28ad1ff44f8fc2b43c0f06e9f02b9dad
Miten <miten2006@gmail.com>
2024-03-17 15:06:14 +0530

    hotel

---
+++ hook/fetchHotel.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+const fetchHotles = () => {
+    const [hotels, setHotels] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.get('http://192.168.0.151:5003/api/places?limit=4');
+            if (response.status === 200) {
+                setHotels(response.data.places);
+                console.log("API call success:", response.status);
+            } else {
+                console.error("API call unsuccessful:", response.status);
+            }
+        } catch (error) {
+            setError(error);
+            console.error("API call unsuccessful:  ", error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    };
+
+    return { recommendations, isLoading, error, refetch };
+};
+
+export default fetchHotles;
+
